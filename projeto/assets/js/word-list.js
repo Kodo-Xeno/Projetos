37f77d603b49d654f7f836851cff8bde6986e087
Leonardo da Silva Lira em vocação <leoslira13@gmail.com>
2025-05-26 14:01:02 -0300

    O projeto do Jogo da Forca, finalizado e alterado
    O último trabalho do curso de Web Design, personalizado.

---
+++ projeto/assets/js/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+{
+    word: "CHAR",
+    hint: "Conjunto de Capacidades que desenvolvemos profissionalmente seguindo um acrônimo"
+},
+// quatro perguntas, RH e Web Design, seja criativo Léo!
+
+{
+    word: "VT",
+    hint: "Benefício que permite o deslocamento com fundos corporativos"
+},
+
+{
+    word: "DP",
+    hint: "Área de logística dentro do RH, tratando de cosias como folha de pagamento"
+},
+
+{
+    word: "Organizacional",
+    hint: "Visão, Missão e Propósito são partes da Cultura . . ."
+},
+]
