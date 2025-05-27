ff5129c1ffd8c71e5e1cb43cca85bae6df81a41c
Leonardo da Silva Lira em vocação <leoslira13@gmail.com>
2025-05-27 14:06:28 -0300

    Finalizado, e pronto para a apresentação,

---
+++ projeto/projeto/projeto/assets/js/word-list.js
@@ -0,0 +1,38 @@
+const wordList = [
+{
+    word: "CHAR",
+    hint: "Conjunto de Capacidades que desenvolvemos profissionalmente seguindo um acrônimo..."
+},
+// quatro perguntas, RH e Web Design, seja criativo Léo!
+
+{
+    word: "VT",
+    hint: "Benefício dado pela empresa para auxiliar o colaborador à se locomover e se transportar..."
+},
+
+{
+    word: "DP",
+    hint: "Área técnica dentro do RH, tratando de assuntos técnicos e competentes de seus colaboradores, como a folha de pagamento"
+},
+
+{
+    word: "Organizacional",
+    hint: "Visão, Missão e Propósito são partes da Cultura . . ."
+},
+{
+    word: "Hexadecimal",
+    hint: "A forma de reconhecimento das cores (Como RGB), deve ser transcrita para..."
+},
+{
+    word: "CSS",
+    hint: "A linguagem que nos auxilia a personalizar a página HTML é o..."
+},
+{
+    word: "HTML",
+    hint: "A linguagem que nos auxilia a criar a estrutura da página é o..."
+},
+{
+    word:"camelCase",
+    hint: "Convenções de nomenclatura, como snake_case ou kebab case, o que mais utilizamos durante o curso foi..."
+}
+]
