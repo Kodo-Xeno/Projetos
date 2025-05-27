14f7da3b9b417dfd6e9d0ddf5b6b97c028c83e7b
Leonardo da Silva Lira em vocação <leoslira13@gmail.com>
2025-05-26 23:21:13 -0300

    Adição remota, adição, correção e finalização
    Finalizei o projeto em casa, em fim adicionei os toques finais como 4 novas palavras  ao jogo

---
+++ projeto/projeto/assets/js/word-list.js
@@ -0,0 +1,34 @@
+const wordList = [
+{
+    word: "CHAR",
+    hint: "Conjunto de Capacidades que desenvolvemos profissionalmente seguindo um acrônimo"
+},
+// quatro perguntas, RH e Web Design, seja criativo Léo!
+
+{
+    word: "VT",
+    hint: "Benefício fornecido pela empresa que permite o deslocamento com fundos corporativos"
+},
+
+{
+    word: "DP",
+    hint: "Área técnica dentro do RH, tratando de coisas como folha de pagamento e detalhes de contratação"
+},
+
+{
+    word: "Organizacional",
+    hint: "Visão, Missão e Propósito são partes da Cultura . . ."
+},
+{
+    word: "Hexadecimal",
+    hint: "A conversão da reconhecimento das cores deve ser transcrita para..."
+},
+{
+    word: "CSS",
+    hint: "A linguagem que nos auxilia a personalizar a página HTML é o..."
+},
+{
+    word: "HTML",
+    hint: "A linguagem que nos auxilia a criar a estrutura da página é o..."
+},
+]
